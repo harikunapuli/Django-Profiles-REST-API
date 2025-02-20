d242b2f8571259fba436f49e4c8a5ec9f169310e
harikunapuli <kunapuliharishankar@gmail.com>
2025-02-20 20:07:40 +0530

    Created Viewsets for Profiles API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path # type: ignore
+from django.urls import path, include # type: ignore
+from rest_framework.routers import DefaultRouter # type: ignore
 from profiles_api import views
 
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
 ]
