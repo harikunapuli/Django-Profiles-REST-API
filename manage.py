62ac94255cc53eab70d9cee08a38dbcaf29c0eb0
harikunapuli <kunapuliharishankar@gmail.com>
2025-02-23 11:23:12 +0530

    manage.py django was resinatalled

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profiles_project.settings')
     try:
-        from django.core.management import execute_from_command_line # type: ignore
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
